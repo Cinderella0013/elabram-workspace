78d884b91e6ae2c60e80b7a2ec128dfb6a95bdba
Cinderella2004 <112455286+Cinderella0013@users.noreply.github.com>
2026-05-14 09:35:18 +0700

    Modify next.config.ts for static output and base path
    Updated Next.js configuration for static export and base path.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // สำคัญ: เพื่อให้ build ออกมาเป็นไฟล์ static
+  basePath: '/elabram-workspace', // ใส่ชื่อ repo ของคุณ
+  images: {
+    unoptimized: true, // GitHub Pages ไม่รองรับ Image Optimization ของ Next.js
+  },
 };
 
 export default nextConfig;
