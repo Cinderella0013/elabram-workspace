f97266966bb75c1f64e5ecab714675cb56e3eccd
Cinderella2004 <112455286+Cinderella0013@users.noreply.github.com>
2026-05-14 10:59:26 +0700

    Enable static HTML export in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export', // สั่งให้ Export เป็น Static HTML
+  output: 'export', // สั่งให้ Export เป็น Static HTML
   basePath: '/elabram-workspace', // ต้องตรงกับชื่อ Repository ของคุณ
   images: {
     unoptimized: true, // GitHub Pages ไม่รองรับระบบแต่งรูปอัตโนมัติของ Next.js
