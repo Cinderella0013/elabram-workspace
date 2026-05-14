f46d21fb9caa0392fda4ecbbda035886f4679e24
Cinderella2004 <112455286+Cinderella0013@users.noreply.github.com>
2026-05-14 11:57:03 +0700

    Remove basePath and images settings from config
    Removed basePath and images configuration from nextConfig.

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/elabram-workspace', // ต้องตรงกับชื่อ Repository ของคุณ
-  images: {
-    unoptimized: true, // GitHub Pages ไม่รองรับระบบแต่งรูปอัตโนมัติของ Next.js
-  },
 };
 
 export default nextConfig;
